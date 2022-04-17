4752c88210f2c6888680527baef1284590718202
Hupaul <hupaulcamacho@pursuit.org>
2022-04-17 11:43:19 -0400

    reformat charData json file and implement getting a single character by their ID

--- controllers/charController.js
+++ controllers/charController.js
@@ -7,4 +7,12 @@ router.get("/", (request, response) => {
     response.json(charData)
 });
 
+router.get("/:id", (request, response) => {
+    const charId = request.params.id;
+    const characters = charData.characters;
+
+    const singleChar = characters.find(char => char.id = charId)
+    response.json(singleChar)
+});
+
 module.exports = router
