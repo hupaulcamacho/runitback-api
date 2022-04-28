1015be4456fde7d4875120f4518c3dd845226594
Hupaul <hupaulcamacho@pursuit.org>
2022-04-28 17:33:48 -0400

    implement get user by id and add new/delete user routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const db = require('../db/dbconfig');
 
 // get all users
-router.get("/", async (request, response) => {
+router.get('/', async (request, response) => {
     try {
         let users = await db.any('SELECT * FROM users');
         response.json(users);
@@ -15,4 +15,56 @@ router.get("/", async (request, response) => {
     }
 });
 
+// get user by id
+router.get('/:id', async (request, response) => {
+    const userId = request.params.id
+    try {
+        let user = await db.one('SELECT * FROM users WHERE id=$1', userId)
+        response.json({
+            message: 'User retrieved...',
+            payload: user,
+        })
+    } catch (error) {
+        console.log(error);
+        response.json({
+            message: 'Unable to retrieve user...',
+        })
+    }
+})
+
+//create a new user
+router.post('/new', async (request, response) => {
+    const { username, email, region, controller } = request.body
+    try {
+        db.none('INSERT INTO users (username, email, region, controller) VALUES ($1, $2, $3, $4)', [username, email, region, controller]);
+
+        response.send({
+            message: 'user created',
+            user: { username, email, region, controller }
+        });
+    } catch (error) {
+        console.log(error)
+        response.json({
+            message: 'Unable to create user'
+        });
+    }
+});
+
+// delete user
+router.delete('/delete/:id', async (request, response) => {
+    const userId = request.params.id
+    try {
+        let user = await db.one('SELECT * FROM users WHERE id=$1', userId)
+        db.oneOrNone('DELETE FROM users WHERE id = $1', [userId])
+        response.json({
+            message: 'user deleted...',
+            user
+        })
+    } catch (error) {
+        console.log(error)
+        response.json({
+            message: 'Unable to delete user'
+        });
+    }
+})
 module.exports = router;
