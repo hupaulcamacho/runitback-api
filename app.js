eb9b58aad8decc8f323dd9f12af6c97c78c831bf
Hupaul <hupaulcamacho@pursuit.org>
2022-05-02 12:17:08 -0400

    add in directory name forr heroku

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, '..', 'build')))
 
 // controllers
 const charController = require("./controllers/charController");
