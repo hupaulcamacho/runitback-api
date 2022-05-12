e21373939dedf48028888d81744de7b6910f5e3d
Hupaul <hupaulcamacho@pursuit.org>
2022-05-12 18:17:05 -0400

    fix user routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllUsers } = require('../queries/usersQueries');
-
-
-router.get("/", async (request, response) => {
+router.get("/",  (request, response) => {
     try {
-        const users = await getAllUsers();
+        const users = [];
         response.json(users);
     } catch(err){
         response.status(500).send('error')
