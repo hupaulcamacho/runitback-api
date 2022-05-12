cb70bef52345f39f081cd6d7d7fe6db4cb17c500
Hupaul <hupaulcamacho@pursuit.org>
2022-05-12 18:11:14 -0400

    complete users route 1

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllUsers } = require('../queries/usersQueries');
-
-
 router.get("/", async (request, response) => {
     try {
-        const users = await getAllUsers();
+        const users =([{name: "jim"},{name: "lop"}]).json();
         response.json(users);
     } catch(err){
         response.status(500).send('error')
