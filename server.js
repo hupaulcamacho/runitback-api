d59768cfc7f236311a379d9b5f5de869dd346815
Hupaul <hupaulcamacho@pursuit.org>
2022-05-12 20:48:39 -0400

    fix user routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
     console.log(`Application is running at port ${PORT}`);
